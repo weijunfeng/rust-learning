52b766138e9e95e6c558683408c3969bf95fe46d
weijunfeng <891130789@qq.com>
2024-07-14 13:55:34 +0800

    简单的 web server内容

--- simple-web-server/src/lib.rs
+++ simple-web-server/src/lib.rs
@@ -1,14 +1,98 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::Receiver;
+use std::thread;
+
+/// 线程池
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// 工作者
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+/// 线程池实现
+impl ThreadPool {
+    /// 创建一个线程池
+    ///
+    /// `size`: 线程池的数量
+    ///
+    /// # Panics
+    ///
+    /// new 函数在 size 为 0时会 panic
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for i in 0..size {
+            workers.push(Worker::new(i, Arc::clone(&receiver)))
+        }
+        return ThreadPool {
+            workers,
+            sender: Some(sender),
+        };
+    }
+
+    /// 在另外一个线程中， 执行 `f` 回调
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        match self.sender.as_ref() {
+            None => {}
+            Some(sender) => sender.send(job).unwrap(),
+        }
+        // self.sender.send(job).unwrap()
+    }
+}
+
+/// 处理ThreadPool销毁，保证线程池中线程都已执行完
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // 销毁发送者，这样 worker中的接收者会接收到一个异常
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap()
+            }
+        }
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            //recv 会阻塞当前线程
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job) => {
+                    println!("worker {id} receive job");
+                    job()
+                }
+                Err(_) => {
+                    println!("worker {id} thread end");
+                    break;
+                }
+            }
+            // let job = receiver.lock().unwrap().recv().unwrap();
+            // job();
+        });
+        // 使用 while let 或 if let或 match 直到相关的代码块结束都才会丢弃临时值， 即lock()的锁在 job调用期间一直持续，其他 worker无法接受到任务
+        // while let Ok(job) = receiver.lock().unwrap().recv() {
+        //     println!("Worker {id} got a job; executing.");
+        //
+        //     job();
+        // }
+        return Worker {
+            id,
+            thread: Some(thread),
+        };
     }
 }
--- simple-web-server/src/main.rs
+++ simple-web-server/src/main.rs
@@ -1,20 +1,61 @@
-use std::io::{BufRead, BufReader};
+use std::{fs, thread};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+
+use simple_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connect(stream);
+    let thread_pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(2) {
+        match stream {
+            Ok(stream) => {
+                thread_pool.execute(|| {
+                    handle_connect(stream);
+                });
+            }
+            Err(e) => {
+                println!("stream error {e}")
+            }
+        }
+    }
+    println!("shutting down");
+}
+
+/// 处理一个链接
+fn handle_connect(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    // println!("request:{http_request:#?}");
+    // 获取请求的第一个行，不处理全部请求内容
+    let first_line = buf_reader.lines().next();
+    match first_line {
+        None => {
+            println!("no request")
+        }
+        Some(lines) => {
+            let (status_line, path) = match &lines.unwrap()[..] {
+                "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+                "GET /sleep HTTP/1.1" => {
+                    thread::sleep(Duration::from_secs(5));
+                    ("HTTP/1.1 200 OK", "hello_sleep.html")
+                }
+                _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+            };
+            let response = build_response(status_line, path);
+            stream.write_all(response.as_bytes()).unwrap();
+        }
     }
 }
 
-fn handle_connect(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request:{http_request:#?}")
+/// 创建一个响应内容
+fn build_response(status_line: &str, path: &str) -> String {
+    let contents = fs::read_to_string(path).unwrap();
+    let length = contents.len();
+    return format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 }
